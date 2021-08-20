c6fcf2dbfb01cda36749bfecec50c08daeedf73f
Adam Donnell <ads2996@googlemail.com>
2021-08-20 23:02:24 +0100

    revert to apt-get

--- install-rickjms-tools.sh
+++ install-rickjms-tools.sh
@@ -9,30 +9,30 @@ function info(){
 }
 
 function install_packages(){
-	# Required for silencing the apt output
+	# Required for silencing the apt-get output
 	sudo DEBIAN_FRONTEND=noninteractive
 	info "Installing required apt depedencies"
 	info "openssl-dev"
-	sudo apt install -qq libcurl4-openssl-dev < /dev/null > /dev/null
-	sudo apt install -qq libssl-dev < /dev/null > /dev/null
+	sudo apt-get install -qq libcurl4-openssl-dev < /dev/null > /dev/null
+	sudo apt-get install -qq libssl-dev < /dev/null > /dev/null
 	info "jq"
-	sudo apt install -qq jq < /dev/null > /dev/null
+	sudo apt-get install -qq jq < /dev/null > /dev/null
 	info "ruby"
-	sudo apt install -qq ruby-full < /dev/null > /dev/null
+	sudo apt-get install -qq ruby-full < /dev/null > /dev/null
 	info "libs"
-	sudo apt install -qq libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev < /dev/null > /dev/null
+	sudo apt-get install -qq libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev < /dev/null > /dev/null
 	info "build"
-	sudo apt install -qq build-essential libssl-dev libffi-dev python-dev < /dev/null > /dev/null
+	sudo apt-get install -qq build-essential libssl-dev libffi-dev python-dev < /dev/null > /dev/null
 	info "libdns"
-	sudo apt install -qq libldns-dev < /dev/null > /dev/null
+	sudo apt-get install -qq libldns-dev < /dev/null > /dev/null
 	info "python3 pip"
-	sudo apt install -qq python3-pip < /dev/null > /dev/null
+	sudo apt-get install -qq python3-pip < /dev/null > /dev/null
 	info "python3 venv"
-	sudo apt install -qq python3-venv < /dev/null > /dev/null
+	sudo apt-get install -qq python3-venv < /dev/null > /dev/null
 	info "git"
-	sudo apt install -qq git < /dev/null > /dev/null
+	sudo apt-get install -qq git < /dev/null > /dev/null
 	info "rename"
-	sudo apt install -qq rename < /dev/null > /dev/null
+	sudo apt-get install -qq rename < /dev/null > /dev/null
 	#info "xargs"
 	#sudo apt install -y -qq xargs
 }
