d75fce8ed9837dad775e2996af8fa9d9fe266265
Adam Donnell <ads@shield.ninja>
2021-08-21 23:05:31 +0100

    better naming for log messages

--- install-rickjms-tools.sh
+++ install-rickjms-tools.sh
@@ -3,12 +3,12 @@
 
 CURRENT_PATH=$(pwd)
 
-function log_h1(){
+function log_title(){
 	local msg="$1"
 	echo -e "\e[0;34m [+] $msg \e[0m"
 }
 
-function log_h2(){
+function log_message(){
 	local msg="$1"
 	echo -e "\e[0;35m  - $msg \e[0m"
 }
@@ -16,27 +16,27 @@ function log_h2(){
 function install_packages(){
 	# Required for silencing the apt-get output
 	DEBIAN_FRONTEND=noninteractive
-	log_h1 "Installing required apt depedencies"
-	log_h2 "openssl-dev"
+	log_title "Installing required apt depedencies"
+	log_message "openssl-dev"
 	sudo apt-get install -qq libcurl4-openssl-dev < /dev/null > /dev/null
 	sudo apt-get install -qq libssl-dev < /dev/null > /dev/null
-	log_h2 "jq"
+	log_message "jq"
 	sudo apt-get install -qq jq < /dev/null > /dev/null
-	log_h2 "ruby"
+	log_message "ruby"
 	sudo apt-get install -qq ruby-full < /dev/null > /dev/null
-	log_h2 "libs"
+	log_message "libs"
 	sudo apt-get install -qq libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev < /dev/null > /dev/null
-	log_h2 "build"
+	log_message "build"
 	sudo apt-get install -qq build-essential libssl-dev libffi-dev python-dev < /dev/null > /dev/null
-	log_h2 "libdns"
+	log_message "libdns"
 	sudo apt-get install -qq libldns-dev < /dev/null > /dev/null
-	log_h2 "python3 pip"
+	log_message "python3 pip"
 	sudo apt-get install -qq python3-pip < /dev/null > /dev/null
-	log_h2 "python3 venv"
+	log_message "python3 venv"
 	sudo apt-get install -qq python3-venv < /dev/null > /dev/null
-	log_h2 "git"
+	log_message "git"
 	sudo apt-get install -qq git < /dev/null > /dev/null
-	log_h2 "rename"
+	log_message "rename"
 	sudo apt-get install -qq rename < /dev/null > /dev/null
 	# Removing as not in ubuntu repos, is installed by default
 	#info "xargs"
@@ -48,19 +48,19 @@ GO_VERSION="go1.16.5.linux-amd64.tar.gz"
 GO_LINK="https://golang.org/dl/$GO_VERSION"
 
 function install_go(){
-	log_h1 "Installing go"
+	log_title "Installing go"
 	wget -q $GO_LINK
-	log_h2 "Uninstalling previous golang installation (if installed) and reinstalling."
+	log_message "Uninstalling previous golang installation (if installed) and reinstalling."
 	sudo rm -rf /usr/local/go && tar -C /usr/local -xzf $GO_VERSION
 	sudo rm -f $GO_VERSION
 	# Check if go path has been added
 	if grep -Fxq "export PATH=$PATH:/usr/local/go/bin" $BASHRC_FILE
 	then
 		# export found
-		log_h2 "Go path alredy in bashrc"
+		log_message "Go path alredy in bashrc"
 	else
 		# export not found
-		log_h2 'export PATH=$PATH:/usr/local/go/bin' >> $BASHRC_FILE
+		log_message 'export PATH=$PATH:/usr/local/go/bin' >> $BASHRC_FILE
 		# Set the export for future functions in this script
 		export PATH=$PATH:/usr/local/go/bin
 	fi
@@ -87,42 +87,42 @@ function update_bashrc(){
 
 
 function install_go_tools(){
-	log_h1 "Installing go tools"
-	log_h2 "assetfinder"
+	log_title "Installing go tools"
+	log_message "assetfinder"
 	go get -u  github.com/tomnomnom/assetfinder
-	log_h2 "httprobe"
+	log_message "httprobe"
 	go get -u github.com/tomnomnom/httprobe
-	log_h2 "subfinder"
+	log_message "subfinder"
 	GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
-	log_h2 "hakrawler"
+	log_message "hakrawler"
 	go get github.com/hakluke/hakrawler
-	log_h2 "waybackurls"
+	log_message "waybackurls"
 	go get github.com/tomnomnom/waybackurls
-	log_h2 "subjack"
+	log_message "subjack"
 	go get github.com/haccer/subjack
-	log_h2 "fff"
+	log_message "fff"
 	go get -u github.com/tomnomnom/fff
-	log_h2 "anew"
+	log_message "anew"
 	go get -u github.com/tomnomnom/anew
-	log_h2 "gobuster"
+	log_message "gobuster"
 	go install github.com/OJ/gobuster/v3@latest
-	log_h2 "go dirsearch"
+	log_message "go dirsearch"
 	go get github.com/evilsocket/dirsearch
 }
 
 
 # This is not 100% might need to get reworked!
 function install_git_tools() {
-	log_h1 "Cloning github repos"
+	log_title "Cloning github repos"
 	GITHUB_DIR="$CURRENT_PATH/github_repos"
 	if [ -d "$GITHUB_DIR" ]; then
-		log_h2 "Not creating github repo dir already exists"
+		log_message "Not creating github repo dir already exists"
 	else
-		log_h2 "Creating $GITHUB_DIR"
+		log_message "Creating $GITHUB_DIR"
 		mkdir $GITHUB_DIR
 	fi
 	
-	log_h2 "sublister"
+	log_message "sublister"
 	if [ -d $GITHUB_DIR/sublister ]; then
 		git pull $GITHUB_DIR/sublister --allow-unrelated-histories
 	else
@@ -134,7 +134,7 @@ function install_git_tools() {
 		sudo ln -s $GITHUB_DIR/sublister/sublist3r.py /bin/sublist3r
 	fi
 
-	log_h2 "tomnomnom hacks"
+	log_message "tomnomnom hacks"
 	if [ -d $GITHUB_DIR/hacks ]; then
 		git pull $GITHUB_DIR/hacks --allow-unrelated-histories
 	else
@@ -142,7 +142,7 @@ function install_git_tools() {
 	fi
 
 	# Install inscope
-	log_h1 "Installing inscope"
+	log_title "Installing inscope"
 	cd $GITHUB_DIR/hacks/inscope
 	go mod init inscope
 	go mod tidy
